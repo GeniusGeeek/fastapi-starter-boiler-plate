67578f42555b7a1a2616c73989e3d28780213cc7
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-08-29 17:37:51 +0100

    Update signin.py

--- controller_routes/signin.py
+++ controller_routes/signin.py
@@ -1,14 +1,10 @@
-# from fastapi import Depends, APIRouter, HTTPException
-# from sqlalchemy.orm import Session
-# from models import orm_model,schema_model
-# from controller_model import signin
-# from database_conn import *
-# orm_model.Base.metadata.create_all(bind=engine)
 from myutils.master_imports import *
 from controller_model import  signin
 
 from fastapi.security import OAuth2PasswordRequestForm
 from myutils import app  
+import requests
+
 
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 300
@@ -35,3 +31,27 @@ def login(user_login_details: OAuth2PasswordRequestForm = Depends(), db: Session
 
   else:  
     raise HTTPException(status_code=400, detail="email does not exist")
+
+
+@router.post("/gmail_auth_signin", summary="user gmail auth login" )
+def gmail_login(token_id: str, db: Session = Depends(get_db)):
+  response = requests.get("https://oauth2.googleapis.com/tokeninfo?id_token="+ token_id)
+  res = response.json()
+  if(res.get("error") is not None):
+      return {"message":"Invalid Id Token"}
+  else:
+
+   login_user = signin.login_email(res['email'],db)
+   #return login_user
+
+   if(login_user):
+       data = {"sub": login_user.id}
+       jwt_exp_time = app.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+       encoded_jwt = app.create_jwt_token(data, jwt_exp_time)
+       return {"access_token": encoded_jwt, "message": "login successfull", "user_details": login_user, "token type": "bearer"}
+
+     
+   else:  
+     raise HTTPException(status_code=400, detail="email does not exist")
+
+
