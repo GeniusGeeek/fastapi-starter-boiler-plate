60bd3c06f09845d67823c874bf543bb7b246971d
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-08-29 17:37:15 +0100

    Update signup.py
    added google authentication

--- controller_routes/signup.py
+++ controller_routes/signup.py
@@ -1,6 +1,9 @@
 from controller_model import signup
 from myutils.master_imports import *
 from fastapi import FastAPI
+import requests
+from enum import Enum
+
 
 
 
@@ -19,3 +22,21 @@ def register(user: schema_model.User, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     else:
         return signup.create_user(user, db)
+
+@router.post('/gmail_auth_signup/{token_id}')
+def gmail_register(token_id: str, db: Session = Depends(get_db)):
+    response = requests.get("https://oauth2.googleapis.com/tokeninfo?id_token="+ token_id)
+    res = response.json()
+    if(res.get("error") is not None):
+      return {"message":"Invalid Id Token"}
+    else:
+      create_user = signup.check_email_exist(db, res['email'])
+      if(create_user):
+        raise HTTPException(status_code=400, detail="Email already registered")
+      else:
+        class googleUser(str, Enum):
+            name = res['name']
+            username = res['given_name']
+            email = res['email']
+        return signup.create_user_from_google_auth(googleUser, db)
+       
