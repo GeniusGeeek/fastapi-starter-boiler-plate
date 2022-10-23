918a167990415fc6d3905244ba8cad7ed07acb3d
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-10-23 11:31:14 +0100

    Update and rename protected_route.py to authenticated_route.py

--- controller_routes/authenticated_route.py
+++ controller_routes/authenticated_route.py
@@ -8,7 +8,7 @@ from myutils.app import auth_user_request
 router = APIRouter()
 
 
-@router.post("/protected_route", summary="A protected route")
+@router.post("/authenticated_route", summary="A protected/authenticated route")
 def post_data(details: schema_model.user_login, db: Session = Depends(get_db), request=Depends(auth_user_request)):
   return {"request_auth_user": request, "inputs": details}
   
