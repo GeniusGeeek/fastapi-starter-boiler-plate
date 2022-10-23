099c34e1a94e981e99e043c4fb29bc0f3657742b
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-10-23 11:32:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
-from controller_routes import protected_route, app_utils, signup, signin, forget_password, reset_password
+from controller_routes import authenticated_route, app_utils, signup, signin, forget_password, reset_password
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(signup.router)
 app.include_router(signin.router)
-app.include_router(protected_route.router)
+app.include_router(authenticated_route.router)
 app.include_router(forget_password.router)
 app.include_router(reset_password.router)
 app.include_router(app_utils.router)
