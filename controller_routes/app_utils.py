c61d490de345aa504c9bbf2c408e679f97f4a573
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-10-23 11:29:09 +0100

    update and renamed file to app_utils
    this file will contain all util features to make the boiler plater more hassle-free for development

---
+++ controller_routes/app_utils.py
@@ -0,0 +1,30 @@
+from myutils.master_imports import *
+
+from myutils import app
+from fastapi import Form
+
+
+
+
+
+router = APIRouter()
+
+
+
+@router.post("/uploadFile", summary="Upload file along with other form data")
+def file_upload(description: str = Form(),image_file=Depends(app.upload_file), db: Session = Depends(get_db), request=Depends(app.auth_user_request)):
+  if (image_file['message'] == "success"):
+      return {"message":"Uploaded Successfuly","uploadpath":image_file['file_path']}
+  else:
+      return {"message":"error occured"+ str(image_file)}
+  
+
+@router.post("/send_email", summary="Send email")
+def send_mail(request=Depends(app.auth_user_request)):
+    response = app.send_mail("mail server","senderemail@mail.com","sender email password","receipient email","message", "subject")
+    return {"message": response}
+  
+    
+  
+  
+  
