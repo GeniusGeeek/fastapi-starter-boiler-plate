72333ade27da70ee274097b9dcd5efb3640b2200
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-11-09 18:01:08 +0100

    Update app_utils.py

--- controller_routes/app_utils.py
+++ controller_routes/app_utils.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 
 
-@router.post("/uploadFile", summary="Upload file along with other form data")
+@router.post("/uploadFile", summary="Upload file along with other form data", tags=["App Utils"])
 def file_upload(description: str = Form(),image_file=Depends(app.upload_file), db: Session = Depends(get_db), request=Depends(app.auth_user_request)):
   if (image_file['message'] == "success"):
       return {"message":"Uploaded Successfuly","uploadpath":image_file['file_path']}
@@ -19,7 +19,7 @@ def file_upload(description: str = Form(),image_file=Depends(app.upload_file), d
       return {"message":"error occured"+ str(image_file)}
   
 
-@router.post("/send_email", summary="Send email")
+@router.post("/send_email", summary="Send email", tags=["App Utils"])
 def send_mail(request=Depends(app.auth_user_request)):
     response = app.send_mail("mail server","senderemail@mail.com","sender email password","receipient email","message", "subject")
     return {"message": response}
