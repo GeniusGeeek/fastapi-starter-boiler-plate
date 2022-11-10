8b535eca2239e4c8b022067564a9a15d1d07aa19
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-11-10 16:53:35 +0100

    Update forget_password.py

--- controller_routes/forget_password.py
+++ controller_routes/forget_password.py
@@ -1,6 +1,7 @@
 from myutils.master_imports import *
 from controller_model import  signin
 from myutils import app  
+import random
 
 
 
@@ -9,12 +10,15 @@ from myutils import app
 
 router = APIRouter()
 
-@router.post("/forget_password", summary="user forget password",tags=["User Account"] )
+
+@router.post("/forget_password", summary="user forget password", tags=["User Account"])
 def post_data(post_data_in: schema_model.forgotPassword, db: Session = Depends(get_db)):
   check_email = signin.login_email(post_data_in.email, db)
-  reset_code = check_email.account_hash
+  #reset_code = check_email.account_hash
+  reset_code = check_email.email_otp
 
   if(check_email):
+    signin.forgotPassModel(post_data_in.email, db)
     if(app.send_mail("mailserver", "sender address", "sender password", post_data_in.email, "Hi this is your reset code: "+reset_code, "Subject: Reset Code") == "sent mail"):
       return {"message": "Reset link successfull sent to your email"}
 
