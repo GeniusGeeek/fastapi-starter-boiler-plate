756f5d654de35860f2f272ab2b2e505039ada29c
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-11-15 15:57:36 +0100

    added gettting static files

--- controller_routes/app_utils.py
+++ controller_routes/app_utils.py
@@ -24,6 +24,15 @@ def send_mail(request=Depends(app.auth_user_request)):
     response = app.send_mail("mail server","senderemail@mail.com","sender email password","receipient email","message", "subject")
     return {"message": response}
   
+  
+@router.get("/staticfiles", summary="get and download static files", tags=["App Utils"])
+def static_files(filename:str):
+    #file_path = os.path.join(path, "uploadedDir/filename.jpg")
+    #if os.path.exists(file_path):
+        #return FileResponse(file_path, media_type="image/jpeg", filename="mycat.jpg")
+    #return {"error": "File not found!"}
+    return FileResponse("uploads/"+filename)
+  
     
   
   
