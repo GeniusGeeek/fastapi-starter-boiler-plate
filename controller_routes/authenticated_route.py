cedf50743011796925c355aae397c400e66de725
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2022-12-15 13:31:05 +0100

    added admin check

--- controller_routes/authenticated_route.py
+++ controller_routes/authenticated_route.py
@@ -12,4 +12,13 @@ router = APIRouter()
 def post_data(details: schema_model.user_login, db: Session = Depends(get_db), request=Depends(auth_user_request)):
   user_unique_id = getUserDetails(request['sub'], "unique_id", db)
   return {"request_auth_user": request, "inputs": details, "user unique_id detail": user_unique_id}
+
+   #is_admin == 1 to check if user is an admin
+  admin_check = getUserDetails(request['sub'], "is_admin", db) 
+  if (admin_check == 1):  
+    return {"request_auth_user": request, "inputs": details, "user unique_id detail": user_unique_id}
+
+  else:
+    return{"message":"Invalid user made this request"}
+  
   
