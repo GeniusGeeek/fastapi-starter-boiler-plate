d1c25a04d9768eec4b03d488ded6262377c0a355
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2023-03-12 16:28:34 +0100

    Update master_imports.py

--- myutils/master_imports.py
+++ myutils/master_imports.py
@@ -4,4 +4,6 @@ from sqlalchemy.orm import Session
 from models import orm_model, schema_model
 from database_conn import *
 orm_model.Base.metadata.create_all(bind=engine)
+domain_name = "127.0.0.1:8000"
+
 
