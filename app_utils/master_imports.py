285a7c489dfa8ad186dc44b7a70de1e66fc8ae4d
Gracious Emmanuel <graciousemmanuel52@gmail.com>
2024-01-28 19:23:12 +0100

    Update master_imports.py

--- app_utils/master_imports.py
+++ app_utils/master_imports.py
@@ -6,5 +6,10 @@ from fastapi.responses import JSONResponse
 from database_conn import *
 orm_model.Base.metadata.create_all(bind=engine)
 domain_name = "http://127.0.0.1:8000"
+email_password = "password"
+email_host = "host.domain.com"
+email_address = "username@domain.com"
+email_port = 465
+some_api_keys = "apikey"
 
 
